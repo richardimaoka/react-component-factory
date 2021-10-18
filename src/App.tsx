e675ece34b11b5c5f148618c03b3b1666fb6fe0f
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-18 22:11:11 +0900

    emotion.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 import React from "react";
 
 function App() {
-  return <div>factory</div>;
+  return (
+    <div
+      css={css`
+        background-color: skyblue;
+      `}
+    >
+      factory
+    </div>
+  );
 }
 
 export default App;
