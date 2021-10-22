a8fe6fc853765fa2309df3df4b2aa202dd67eda1
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-22 16:38:53 +0900

    paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -64,9 +64,22 @@ const PageTitle = ({ title }: PageTitleProps) => (
   </div>
 )
 
+const Paragraph = () => (
+  <p
+    css={css`
+      padding: 4px;
+    `}
+  >
+    Cypressはフロントエンドのend-to-endテストフレームワークの代表格です。ローカル開発環境で
+    npx cypress open
+    コマンドを実行すれば、Cypressはブラウザを自動で立ち上げて、高速で次々にテストを完了していきます。その軽快な動作は非常に心地よく、フロントエンドのテスト駆動開発を推進するのにピッタリなツールです。CIサーバー上ではブラウザを立ち上げることなくheadlessモードで動作します。
+  </p>
+)
+
 const PageElements = () => (
   <div>
     <PageTitle title="概要:cypressはフロントエンドend-to-endテストの代表格、WSLで動かしてみよう！" />
+    <Paragraph />
   </div>
 )
 
