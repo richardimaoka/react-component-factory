ba1ae447027e9dc33e904720ea4820e5f99b9290
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-27 17:07:48 +0900

    file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,59 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { useEffect, useRef } from 'react'
+import Prism from 'prismjs'
+
+interface FileTabProps {
+  filename: string
+}
+
+export const FileTab = ({ filename }: FileTabProps) => (
+  <div
+    css={css`
+      padding-top: 4px;
+      padding-bottom: 6px;
+      padding-right: 8px;
+      padding-left: 8px;
+    `}
+  >
+    {filename}
+  </div>
+)
+
+interface FileContentProps {
+  filecontent: string
+}
+
+export const FileContent = ({ filecontent }: FileContentProps) => {
+  const codeElement = useRef<HTMLElement>(null)
+  useEffect(() => {
+    if (codeElement.current) {
+      Prism.highlightElement(codeElement.current)
+    }
+  })
+
+  return (
+    <div>
+      <pre>
+        <code ref={codeElement} className={'lang-js'}>
+          {filecontent}
+        </code>
+      </pre>
+    </div>
+  )
+}
+
+interface SingleFileProps {
+  filename: string
+  filecontent: string
+}
+
+export const SingleFile = ({ filename, filecontent }: SingleFileProps) => (
+  <div>
+    <FileTab filename={filename} />
+    <FileContent filecontent={filecontent} />
+  </div>
+)
 
 export const MainContainer = () => {
   return (
@@ -11,7 +65,33 @@ export const MainContainer = () => {
           width: 768px;
         `}
       >
-        <div />
+        <SingleFile
+          filename={'main.js'}
+          filecontent={`import { createStore } from 'redux'
+
+/**
+ * This is a reducer - a function that takes a current state value and an
+ * action object describing "what happened", and returns a new state value.
+ * A reducer's function signature is: (state, action) => newState
+ *
+ * The Redux state should contain only plain JS objects, arrays, and primitives.
+ * The root state value is usually an object.  It's important that you should
+ * not mutate the state object, but return a new object if the state changes.
+ *
+ * You can use any conditional logic you want in a reducer. In this example,
+ * we use a switch statement, but it's not required.
+ */
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}`}
+        />
       </div>
     </main>
   )
