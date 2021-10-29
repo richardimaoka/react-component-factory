41b4758f79cee75f11a0b8b8b2735dfe25e058a2
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-29 18:10:29 +0900

    file tab bar

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,28 @@ export const FileTab = ({ filename }: FileTabProps) => (
   </div>
 )
 
+interface FileNameTabBarProps {
+  files: File[]
+  selectFileIndex: number
+}
+
+export const FileNameTabBar = ({
+  files,
+  selectFileIndex,
+}: FileNameTabBarProps) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      {files.map((file, index) => (
+        <FileTab key={index} filename={files[selectFileIndex].filename} />
+      ))}
+    </div>
+  )
+}
+
 interface FileContentProps {
   filecontent: string
 }
@@ -57,32 +79,25 @@ export const FileContent = ({ filecontent }: FileContentProps) => {
   )
 }
 
-interface SingleFileProps {
+interface File {
   filename: string
   filecontent: string
 }
+interface FileViewerProps {
+  files: File[]
+  selectFileIndex: number
+}
 
-export const SingleFile = ({ filename, filecontent }: SingleFileProps) => (
-  <div>
-    <FileTab filename={filename} />
-    <FileContent filecontent={filecontent} />
-  </div>
-)
-
-export const MainContainer = () => {
+export const FileViewer = ({ files, selectFileIndex }: FileViewerProps) => {
   return (
-    <main>
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          width: 768px;
-        `}
-      >
-        <SingleFile
-          filename={'main.js'}
-          filecontent={`import { createStore } from 'redux'
+    <div>
+      <FileNameTabBar files={files} selectFileIndex={selectFileIndex} />
+      <FileContent filecontent={files[selectFileIndex].filecontent} />
+    </div>
+  )
+}
 
+const fileContentMainJs = `import { createStore } from 'redux'
 /**
  * This is a reducer - a function that takes a current state value and an
  * action object describing "what happened", and returns a new state value.
@@ -104,7 +119,79 @@ function counterReducer(state = { value: 0 }, action) {
     default:
       return state
   }
-}`}
+}`
+
+const fileContentPackageJson = `{
+  "name": "my-app",
+  "version": "0.1.0",
+  "private": true,
+  "dependencies": {
+    "@emotion/react": "^11.5.0",
+    "@testing-library/jest-dom": "^5.11.4",
+    "@testing-library/react": "^11.1.0",
+    "@testing-library/user-event": "^12.1.10",
+    "@types/jest": "^26.0.15",
+    "@types/node": "^12.0.0",
+    "@types/react": "^17.0.0",
+    "@types/react-dom": "^17.0.0",
+    "prismjs": "^1.25.0",
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2",
+    "react-scripts": "4.0.3",
+    "web-vitals": "^1.0.1"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "eslintConfig": {
+    "extends": [
+      "react-app",
+      "react-app/jest"
+    ]
+  },
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
+  },
+  "devDependencies": {
+    "@types/prismjs": "^1.16.6",
+    "@typescript-eslint/eslint-plugin": "^4.33.0",
+    "@typescript-eslint/parser": "^4.33.0",
+    "eslint": "^7.32.0",
+    "stylelint": "^13.13.1",
+    "stylelint-config-standard": "^22.0.0",
+    "typescript": "^4.4.4"
+  }
+}
+`
+
+export const MainContainer = () => {
+  return (
+    <main>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          width: 768px;
+        `}
+      >
+        <FileViewer
+          files={[
+            { filename: 'main.js', filecontent: fileContentMainJs },
+            { filename: 'package.json', filecontent: fileContentPackageJson },
+          ]}
+          selectFileIndex={0}
         />
       </div>
     </main>
