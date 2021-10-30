dd8832ad33aef89c1ab5a53f506fc9e4ee07b6f4
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-10-31 06:31:21 +0900

    file and directory

--- src/App.tsx
+++ src/App.tsx
@@ -265,6 +265,85 @@ const fileContentPackageJson = `{
 }
 `
 
+export const FileIcon = (): JSX.Element => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20px"
+        height="20px"
+        css={css`
+          fill: rgb(83, 100, 113);
+        `}
+        viewBox="0 0 40 40"
+      >
+        <g>
+          <rect x="-6" y="-64" width="50" height="108" />
+        </g>
+        <g>
+          <g>
+            <path
+              id="rect1662"
+              d="M9.43,1.5a4,4,0,0,0-4,4V34.54a4,4,0,0,0,4,4H30.57a4,4,0,0,0,4-4V9.43a1.33,1.33,0,0,0-.39-.94l-6.61-6.6a1.31,1.31,0,0,0-.93-.39Zm0,2.64H24v4a4,4,0,0,0,4,4h4V34.54a1.29,1.29,0,0,1-1.32,1.32H9.43a1.29,1.29,0,0,1-1.32-1.32V5.46A1.29,1.29,0,0,1,9.43,4.14Zm17.18.55,4.74,4.74H27.93a1.29,1.29,0,0,1-1.32-1.32Z"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+export const DirectoryIcon = (): JSX.Element => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20px"
+        height="20px"
+        css={css`
+          fill: rgb(83, 100, 113);
+        `}
+        viewBox="0 0 40 40"
+      >
+        <g>
+          <rect x="-6" y="-4" width="50" height="108" />
+        </g>
+        <g>
+          <g>
+            <path d="M5.25,3.91a4,4,0,0,0-4,4V32.07a4,4,0,0,0,4,4h29.5a4,4,0,0,0,4-4V13.29a4,4,0,0,0-4-4H20.65L17.72,5.56a4,4,0,0,0-3.08-1.65Zm0,2.68h9.39c.29,0,.61.18,1,.64L19,11.44A1.31,1.31,0,0,0,20,12H34.75a1.32,1.32,0,0,1,1.34,1.34V32.07a1.3,1.3,0,0,1-1.34,1.34H5.25a1.3,1.3,0,0,1-1.34-1.34V7.93A1.3,1.3,0,0,1,5.25,6.59Z" />
+          </g>
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+interface FileTreeProps {
+  filenames: string[] //it should be recursive file nodes
+}
+
+export const FileTree = ({ filenames }: FileTreeProps): JSX.Element => (
+  <div
+    css={css`
+      padding: 4px;
+      background-color: #2d2d2d;
+    `}
+  >
+    {filenames.map((filename, index) => {
+      return (
+        <div
+          key={index}
+          css={css`
+            color: #ccc;
+          `}
+        >
+          {filename}
+        </div>
+      )
+    })}
+  </div>
+)
+
 export const MainContainer = (): JSX.Element => {
   const { /*loading,*/ error, data } = useQuery(GET_FILESET)
 
@@ -282,6 +361,7 @@ export const MainContainer = (): JSX.Element => {
             width: 768px;
           `}
         >
+          <FileTree filenames={['main.js', 'package.json']} />
           <FileViewer
             files={[
               { filename: 'main.js', filecontent: fileContentMainJs },
