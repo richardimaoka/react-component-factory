c1230b17428687c6794f0f22cd68e64eb085a847
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-26 00:27:58 +0900

    bare bone

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,127 +11,20 @@ const query = gql`
   }
 `
 
-export type TextChunkComponentFragment = {
-  __typename?: 'TextChunk'
-  text: string | null
-  highlight: boolean | null
-  bold: boolean | null
-  hyperlinkUrl: string | null
-  strikeout: boolean | null
-  inlineCode: boolean | null
-}
-
-interface TextChunkProps {
-  fragment: TextChunkComponentFragment
-}
-
-const InnerTextChunkComponent = ({ fragment }: TextChunkProps): JSX.Element => {
-  const cssProperties = css`
-    background-color: ${fragment.highlight ? '#E6FF01' : 'transparent'};
-    font-weight: ${fragment.bold ? 'bold' : 'normal'};
-    text-decoration: ${fragment.strikeout ? 'line-through' : 'no'};
-  `
-
-  return fragment.inlineCode ? (
-    <code
-      css={css`
-        background-color: #252525;
-        color: white;
-      `}
-    >
-      {fragment.text}
-    </code>
-  ) : (
-    <span css={cssProperties}>{fragment.text}</span>
-  )
-}
-
-export const TextChunkComponent = ({
-  fragment,
-}: TextChunkProps): JSX.Element => {
-  if (fragment.hyperlinkUrl) {
-    return (
-      <a href={fragment.hyperlinkUrl}>
-        <InnerTextChunkComponent fragment={fragment} />
-      </a>
-    )
-  } else {
-    return <InnerTextChunkComponent fragment={fragment} />
-  }
-}
-export type ParagraphComponentFragment = {
-  __typename?: 'Paragraph'
-  chunks: Array<{
-    __typename?: 'TextChunk'
-    text: string | null
-    highlight: boolean | null
-    bold: boolean | null
-    hyperlinkUrl: string | null
-    strikeout: boolean | null
-    inlineCode: boolean | null
-  } | null> | null
-}
-
-interface ParagraphProps {
-  fragment: ParagraphComponentFragment
-}
-
-export const ParagraphComponent = ({
-  fragment,
-}: ParagraphProps): JSX.Element => {
-  if (!fragment.chunks) {
-    return <React.Fragment />
-  } else {
-    return (
-      <div
-        css={css`
-          padding: 8px;
-        `}
-        contentEditable
-      >
-        <p
-          css={css`
-            color: #0a0a0a;
-            margin: 0px;
-          `}
-        >
-          {fragment.chunks.map((chunk, index) =>
-            chunk ? (
-              <TextChunkComponent key={index} fragment={chunk} />
-            ) : (
-              <React.Fragment />
-            )
-          )}
-        </p>
-      </div>
-    )
-  }
-}
-
 export const MainContainer = (): JSX.Element => {
   return (
     <main>
       <div
         css={css`
-          width: 750px;
+          display: flex;
+          justify-content: center;
         `}
       >
-        <ParagraphComponent
-          fragment={{
-            __typename: 'Paragraph',
-            chunks: [
-              {
-                __typename: 'TextChunk',
-                text: 'Something There',
-                highlight: null,
-                bold: null,
-                hyperlinkUrl: null,
-                strikeout: null,
-                inlineCode: null,
-              },
-            ],
-          }}
-        />
+        <div
+          css={css`
+            width: 768px;
+          `}
+        ></div>
       </div>
     </main>
   )
