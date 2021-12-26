7022a53c3bc4c5b794202864d4d1e00774d086e1
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-27 06:59:48 +0900

    React testing library barebone

--- client/src/action/Action.test.tsx
+++ client/src/action/Action.test.tsx
@@ -1,5 +1,15 @@
 import { ActionComponent } from './ActionComponent'
+import { render, screen } from '@testing-library/react'
 
-it('just runs', () => {
+it('renders nothing when Action is empty', () => {
+  const jsonString = `{
+    "__typename": "Action"
+  }`
+  const fragment = JSON.parse(jsonString)
+
+  const { container } = render(<ActionComponent fragment={fragment} />)
+  console.log(container.childNodes.length)
+  console.log(container.getBoundingClientRect())
+  screen.debug()
   expect(3).toEqual(3)
 })
