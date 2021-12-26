0683b1c45577378e653605451dea36b11261d757
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-26 21:20:23 +0900

    ActionLabelComponent

---
+++ client/src/ActionLabelComponent.tsx
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+export const ActionLabelComponent = (): JSX.Element => {
+  return (
+    <div
+      css={css`
+        padding: 4px 8px;
+        background-color: #eecf33;
+      `}
+    >
+      Action
+    </div>
+  );
+};
