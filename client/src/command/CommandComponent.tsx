927f6b353a4387fda0484aac66654e38251764e5
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-27 07:03:34 +0900

    empty command handling

--- client/src/command/CommandComponent.tsx
+++ client/src/command/CommandComponent.tsx
@@ -10,36 +10,40 @@ interface CommandComponentProps {
 export const CommandComponent = ({
   fragment,
 }: CommandComponentProps): JSX.Element => {
-  return (
-    <div
-      css={css`
-        position: relative;
-        padding: 8px 8px;
-        background-color: #2d2d2d;
-        color: #ffffff;
-      `}
-    >
-      <pre
+  if (!fragment.text || fragment.text.length === 0) {
+    return <></>
+  } else {
+    return (
+      <div
         css={css`
-          margin: 0px;
-        `}
-      >
-        <code>{fragment.text}</code>
-      </pre>
-      <button
-        css={css`
-          position: absolute;
-          right: 8px;
-          top: 4px;
+          position: relative;
+          padding: 8px 8px;
           background-color: #2d2d2d;
-          border: solid 1px #888888;
           color: #ffffff;
         `}
       >
-        copy
-      </button>
-    </div>
-  )
+        <pre
+          css={css`
+            margin: 0px;
+          `}
+        >
+          <code>{fragment.text}</code>
+        </pre>
+        <button
+          css={css`
+            position: absolute;
+            right: 8px;
+            top: 4px;
+            background-color: #2d2d2d;
+            border: solid 1px #888888;
+            color: #ffffff;
+          `}
+        >
+          copy
+        </button>
+      </div>
+    )
+  }
 }
 
 CommandComponent.fragment = gql`
