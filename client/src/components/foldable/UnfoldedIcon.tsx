fd54a541d9fe96267dcfcde4f49daf83699a2c4f
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-31 01:20:19 +0900

    remove unnecesary div from icons

--- client/src/components/foldable/UnfoldedIcon.tsx
+++ client/src/components/foldable/UnfoldedIcon.tsx
@@ -3,20 +3,18 @@ import { css } from '@emotion/react'
 
 export const UnfoldedIcon = (): JSX.Element => {
   return (
-    <div>
-      <svg
-        width="21px"
-        height="21px"
-        css={css`
-          fill: rgb(83, 100, 113);
-        `}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 40 40"
-      >
-        <g>
-          <path d="M22.53,36.85,38,10.07a2.93,2.93,0,0,0-2.54-4.39H4.54A2.93,2.93,0,0,0,2,10.07L17.47,36.85A2.92,2.92,0,0,0,22.53,36.85Z" />
-        </g>
-      </svg>
-    </div>
+    <svg
+      width="21px"
+      height="21px"
+      css={css`
+        fill: rgb(83, 100, 113);
+      `}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 40 40"
+    >
+      <g>
+        <path d="M22.53,36.85,38,10.07a2.93,2.93,0,0,0-2.54-4.39H4.54A2.93,2.93,0,0,0,2,10.07L17.47,36.85A2.92,2.92,0,0,0,22.53,36.85Z" />
+      </g>
+    </svg>
   )
 }
